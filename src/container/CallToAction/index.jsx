37e7b0962053f7f51c2e5acf940dd2bbb2f4e87b
Rahul Dubey <rahuldubey7398@gmail.com>
2024-06-18 10:10:36 +0530

    forms connection to netlify

--- src/container/CallToAction/index.jsx
+++ src/container/CallToAction/index.jsx
@@ -13,43 +13,48 @@ const CallToAction = () => {
               meal is on us!
             </p>
 
-            <form className="cta-form" action="#">
-              <div>
-                <label htmlFor="full-name">Full Name</label>
-                <input
-                  id="full-name"
-                  type="text"
-                  required
-                  placeholder="John Doe"
-                />
-              </div>
+            {
+              /*  eslint-disable-next-line react/no-unknown-property */
+              <form className="cta-form" name="contact" netlify>
+                <div>
+                  <label htmlFor="full-name">Full Name</label>
+                  <input
+                    id="full-name"
+                    type="text"
+                    required
+                    placeholder="John Doe"
+                    name="full-name"
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="email">Email Address</label>
-                <input
-                  id="email"
-                  type="email"
-                  required
-                  placeholder="abc@example.com"
-                />
-              </div>
+                <div>
+                  <label htmlFor="email">Email Address</label>
+                  <input
+                    id="email"
+                    type="email"
+                    required
+                    placeholder="abc@example.com"
+                    name="email"
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="select-where">
-                  Where did you hear from us?
-                </label>
-                <select id="select-where">
-                  <option value="">Please choose one option:</option>
-                  <option value="friends">Friends and Family</option>
-                  <option value="youtube">Youtube Video</option>
-                  <option value="Podcast">Podcast</option>
-                  <option value="Ad">Facebook Ad</option>
-                  <option value="Others">Others</option>
-                </select>
-              </div>
+                <div>
+                  <label htmlFor="select-where">
+                    Where did you hear from us?
+                  </label>
+                  <select id="select-where" name="id-where">
+                    <option value="">Please choose one option:</option>
+                    <option value="friends">Friends and Family</option>
+                    <option value="youtube">Youtube Video</option>
+                    <option value="Podcast">Podcast</option>
+                    <option value="Ad">Facebook Ad</option>
+                    <option value="Others">Others</option>
+                  </select>
+                </div>
 
-              <button className="btn btn--form">Sign Up Now</button>
-            </form>
+                <button className="btn btn--form">Sign Up Now</button>
+              </form>
+            }
           </div>
           <div
             className="cta-img-box"
